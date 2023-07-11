5bd6e64c8b2221645ffda92a8cf2a68916cdf9e6
Yufeng <yufenghe87@gmail.com>
2023-07-11 00:23:59 -0500

    [switchgrass] 3 changes: add a 2nd site in ND change iSp to 1.1 based on Dohleman's thesis seperate abv ground into leaf and stem

--- switchgrass/code/optimization/objectivefunction_switchgrass.R
+++ switchgrass/code/optimization/objectivefunction_switchgrass.R
@@ -1,4 +1,4 @@
-objectivefunction_switchgrass <- function (parms,soybean_optsolver,observation_switchgrass_IL,wt)
+objectivefunction_switchgrass <- function (parms,soybean_optsolver,optsolver_site2,observation_switchgrass_IL,observation_switchgrass_ND,wt)
 {
          
   date_2006 = c("2006-04-15","2006-06-15","2006-08-15","2006-10-15")
@@ -48,33 +48,62 @@ objectivefunction_switchgrass <- function (parms,soybean_optsolver,observation_s
                    result2008$root[result2008$doy %in% doy_all[[3]]])*(1/3)
 
     Aboveground_pred = avgstem + avgleaf #+ avgleaflitter
+    Leaf_pred    = avgleaf
+    Stem_pred    = avgstem
     Rhizome_pred = avgrhizome
-    Root_pred = avgroot
-   print(Aboveground_pred)
-   #hourly to daily
+    Root_pred    = avgroot
+   #hourly to daily to match OBS's
     Aboveground_pred = colMeans(matrix(as.vector(Aboveground_pred), nrow=24))
+    Leaf_pred        = colMeans(matrix(as.vector(Leaf_pred), nrow=24))
+    Stem_pred        = colMeans(matrix(as.vector(Stem_pred), nrow=24))
     Rhizome_pred     = colMeans(matrix(as.vector(Rhizome_pred), nrow=24))
     Root_pred        = colMeans(matrix(as.vector(Root_pred), nrow=24))
-   print(Aboveground_pred)
-   print(observation_switchgrass_IL$Aboveground)
    #error 
-   AbovegroundE = (observation_switchgrass_IL$Aboveground - Aboveground_pred)
+   LeafE        = (observation_switchgrass_IL$Leaf        - Leaf_pred)
+   StemE        = (observation_switchgrass_IL$Stem        - Stem_pred)
    RootE        = (observation_switchgrass_IL$Root        - Root_pred)
    RhizomeE     = (observation_switchgrass_IL$Rhizome     - Rhizome_pred)
    
-   E = sum(AbovegroundE^2)*wt$aboveground + sum(RootE^2)*wt$root + sum(RhizomeE^2)*wt$rhizome
+   E1 = sum(LeafE^2)*wt$leaf + sum(StemE^2)*wt$stem + sum(RootE^2)*wt$root + sum(RhizomeE^2)*wt$rhizome
+
+   E1 = E1/length(LeafE)
     
    #try avoid overestimation of the LAST aboveground by adding a penalty
    penalty =  max(0,tail(Aboveground_pred - observation_switchgrass_IL$Aboveground,1))
-   E = E + 20 * penalty^2
+   E1 = E1 + 20 * penalty^2
 
-   if(is.na(E)) {
+   if(is.na(E1)) {
      print(parms) 
-     E = 9999  
+     E1 = 9999  
    }
 
+#get the second site's error
+   obs_doys = observation_switchgrass_ND$doy
+   obs_stem = observation_switchgrass_ND$stem
+   obs_leaf = observation_switchgrass_ND$leaf
+   solver <- optsolver_site2[[1]] #2001
+   result2001 <- solver(parms)
+   solver <- optsolver_site2[[2]] #2002
+   result2002 <- solver(parms)
+
+   avgstem =  (result2001$Stem[result2001$doy %in% obs_doys] +
+               result2002$Stem[result2002$doy %in% obs_doys])/2
+    
+   avgleaf =  (result2001$Leaf[result2001$doy %in% obs_doys] +
+               result2002$Leaf[result2002$doy %in% obs_doys])/2
+   #hourly to daily
+   avgstem_pred = colMeans(matrix(as.vector(avgstem), nrow=24))
+   avgleaf_pred = colMeans(matrix(as.vector(avgleaf), nrow=24))
+
+   stemE = avgstem - obs_stem
+   leafE = avgleaf - obs_leaf
+   E2 = sum(stemE^2) + sum(leafE^2)
+   E2 = E2/length(stemE)
+   #print(c(E1,E2))
+   E = E1+E2
 #   diff = result$kLeaf - result$kStem
 #   if(length(which(diff>0)) > 100) E=9999 
+
    #be careful! here is hard-coded indices, corresponding to kLeaf_emr and kStem_emr
    if(parms[8]+parms[9]>1) E=9999  
 
--- switchgrass/code/optimization/run_biocro_switchgrass_optimization_on_biocluster.R
+++ switchgrass/code/optimization/run_biocro_switchgrass_optimization_on_biocluster.R
@@ -5,14 +5,15 @@ run_cwrfsoilwater=TRUE
 
 # reading observation and climate data
 observation_switchgrass_IL = read.csv("../../data/biomass_observation//observations_switchgrass_2006_2008.csv")
+#ND location: 46.77,-100.92
+observation_switchgrass_ND = read.csv("../../data/biomass_observation//lit_data.csv")
 
-# File containing information to map result index and observation corresponding to date of measurements
-#observation_result_mapping <- read.csv("../../data/biomass_observation//switchgrass_observation_result_mapping_for_optimization.csv")
-
+#exlude the last point in Dec.
 observation_switchgrass_IL <- observation_switchgrass_IL[1:4,] 
-#observation_result_mapping <- observation_result_mapping[1:4,]
+print(observation_switchgrass_IL)
 
 weather_urbana = read.csv('../../data/weather/NASA_data/BioCroInputs/site_1_lowerTransmittance.csv')
+weather_ND     = read.csv('../../data/weather/NASA_data/BioCroInputs/site_99_lowerTransmittance.csv')
 
 # Modules/initial conditions required for running biocro
 source("../../data/parameters/ss_modules_list_switchgrass.R")
@@ -26,29 +27,39 @@ source("objectivefunction_switchgrass.R")
 
 # Running optimization for the logistical phase
 parameters_to_optimize <- c("alphaStem","betaStem","alphaLeaf","betaLeaf","alphaRoot","betaRoot",
-                             "kRhizome_emr","kLeaf_emr","kStem_emr","leaf_turnover_rate")
+                             "kRhizome_emr","kLeaf_emr","kStem_emr","leaf_turnover_rate","TTemr")
 #parameters_to_optimize <- c("kRhizome_emr","kLeaf_emr","kStem_emr","leaf_turnover_rate","TTemr")
 parameters_list_switchgrass$TTc_leafsenescence_threshold = 5 
 parameters_list_switchgrass$soil_type_indicator = 4 #YH: this by default was 10!?  
-parameters_list_switchgrass$iSp    = 1.7
-parameters_list_switchgrass$TTemr  = 300 
+parameters_list_switchgrass$iSp    = 1.1
+#parameters_list_switchgrass$TTemr  = 300 
 
 #parameters_list_switchgrass$tbase      = 10
 #parameters_list_switchgrass$topt_upper = 31
 #parameters_list_switchgrass$tmax       = 40 
+parameters_list_switchgrass$upperT  = 28 
 
-years = 2006:2008
+years  = 2006:2008
+years2 = 2001:2002
 if(run_cwrfsoilwater){
   parameters_list_switchgrass$wsFun=0
   deriv_modules_list_switchgrass = deriv_modules_list_switchgrass[-3]
   initial_state_switchgrass = 
   initial_state_switchgrass[names(initial_state_switchgrass)!=c('soil_water_content')]
   cwrf_soilwater = list()
-  k=1
-  for(year in years){
-   soil_data = read.csv(paste0('~/MFEW/switchgrass/site_1/cwrf_soilwater_',year,'.csv'))
-   cwrf_soilwater[[k]] = soil_data[c('doy','swc')] 
-   k=k+1
+  for(i in 1:length(years)){
+   year = years[i]
+#first site's soil water
+   soil_data = read.csv(paste0('~/MFEW_CWRF_soil_water/switchgrass/site_1/cwrf_soilwater_',year,'.csv'))
+   cwrf_soilwater[[i]] = soil_data[c('doy','swc')] 
+  }
+
+  cwrf_soilwater2 = list()
+  for(i in 1:length(years2)){
+   year2 = years2[i]
+#second site's soil water
+   soil_data = read.csv(paste0('~/MFEW_CWRF_soil_water/switchgrass/site_99/cwrf_soilwater_',year2,'.csv'))
+   cwrf_soilwater2[[i]] = soil_data[c('doy','swc')] 
   }
 }else{
   ##turn off water stress
@@ -61,12 +72,10 @@ lower_bound_parameters <- c(0 ,-20,0 ,-20,0 ,-20)
 upper_bound_parameters <- c(20,0  ,20,0  ,20,0  )
 #lower_bound_parameters <- c(-20 ,-20 ,-20 ,-20 ,-20 ,-20)
 #upper_bound_parameters <- c(20  ,20  ,20  ,20  ,20  ,20  )
-lower_bound_parameters <- c(lower_bound_parameters,c(-0.01   ,0.01,0.01,0.0  ))
-upper_bound_parameters <- c(upper_bound_parameters,c(-0.00001,0.80,0.80,0.002))
-#lower_bound_parameters <- c(-0.01   ,0.01,0.01,0.0,  100)
-#upper_bound_parameters <- c(-0.00001,0.80,0.80,0.002,800)
+lower_bound_parameters <- c(lower_bound_parameters,c(-0.01   ,0.01,0.01,0.0  ,100))
+upper_bound_parameters <- c(upper_bound_parameters,c(-0.00001,0.80,0.80,0.002,500))
 
-wt <- list(aboveground = 0.5, root = 1, rhizome = 1)
+wt <- list(leaf=1,stem=1,aboveground = 1, root = 1, rhizome = 1)
 
 solver=list(type='Gro_euler', output_step_size=1.0, adaptive_rel_error_tol=1e-4, adaptive_abs_error_tole=1e-4, adaptive_max_steps=200)
 
@@ -84,7 +93,8 @@ for (i in 1:length(years)){
      tmp = cwrf_soilwater[[i]]
      growing_season$soil_water_content = tmp$swc[tmp$doy>=growing_season$doy[1] & tmp$doy<=tail(growing_season$doy,1)] 
    }else{
-      initial_state_switchgrass$soil_water_content = soilmoisture[[4+i]]
+   #this currently does not work 
+      initial_state_switchgrass$soil_water_content = soilmoisture[[i]]
    }
    optsolver[[i]] <- partial_gro_solver(initial_state =  initial_state_switchgrass,
                         parameters = parameters_list_switchgrass,
@@ -93,8 +103,30 @@ for (i in 1:length(years)){
                         derivative_module_names = deriv_modules_list_switchgrass, parameters_to_optimize,solver=solver,verbose = FALSE)
 }
 
+optsolver2 <- list()
+initial_state_switchgrass$Rhizome = 4.0
+initial_state_switchgrass$Root    = 0.1
+for (i in 1:length(years2)){
+   ind = which(weather_ND$year==years2[i])
+   growing_season = weather_ND[ind,]
+   #growing_season = get_growing_season_climate(growing_season,threshold_temperature = 0)
+   growing_season = growing_season[growing_season$doy>=130 & growing_season$doy<=260,]
+   if(run_cwrfsoilwater){
+     tmp = cwrf_soilwater2[[i]]
+     growing_season$soil_water_content = tmp$swc[tmp$doy>=growing_season$doy[1] & tmp$doy<=tail(growing_season$doy,1)] 
+   }else{
+   
+      initial_state_switchgrass$soil_water_content = soilmoisture[[i]]
+   }
+   optsolver2[[i]] <- partial_gro_solver(initial_state =  initial_state_switchgrass,
+                        parameters = parameters_list_switchgrass,
+                        varying_parameters = growing_season,
+                        steady_state_module_names = ss_modules_list_swithcgrass,
+                        derivative_module_names = deriv_modules_list_switchgrass, parameters_to_optimize,solver=solver,verbose = FALSE)
+}
+
 cost_func <- function(x){
-  objectivefunction_switchgrass(x,optsolver,observation_switchgrass_IL,wt)
+  objectivefunction_switchgrass(x,optsolver,optsolver2,observation_switchgrass_IL,observation_switchgrass_ND,wt)
 }
 
 set.seed(1234)
@@ -102,16 +134,17 @@ set.seed(1234)
 max.iter <- 1000
 
 # Call DEoptim function to run optimization
-parVars <- c('objectivefunction_switchgrass','optsolver','observation_switchgrass_IL',
-                                           'wt')
+parVars <- c('objectivefunction_switchgrass','optsolver','optsolver2',
+              'observation_switchgrass_IL', 'observation_switchgrass_ND','wt')
+
 cl <- makeCluster(8)
 clusterExport(cl, parVars,envir=environment())
 
 optim_result<-DEoptim(fn=cost_func, lower=lower_bound_parameters, upper = upper_bound_parameters, 
-                      control=list(VTR=30,itermax=max.iter,parallelType=1,packages=c('BioCroMis'),cluster=cl))
+                      control=list(VTR=20,itermax=max.iter,parallelType=1,packages=c('BioCroMis'),cluster=cl))
 #optim_result<-DEoptim(fn=cost_func, lower=lower_bound_parameters, upper = upper_bound_parameters, 
-#                      control=list(VTR=30,itermax=max.iter,parallelType=0,packages=c('BioCroMis')))
+#                      control=list(VTR=20,itermax=max.iter,parallelType=0,packages=c('BioCroMis')))
 
 opt_result = data.frame(optim_result$par,MSE=optim_result$value)
 
-saveRDS(opt_result,'opt_result_DEoptim_cwrfsoil_r10.rds')
+saveRDS(opt_result,'opt_result_DEoptim_cwrfsoil_NDsite_r3.rds')
